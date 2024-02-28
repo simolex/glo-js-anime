344f9ff006d5b2dca27d14d0dfd4d7f94b5e3838
simolex <simolex@yandex.ru>
2024-02-28 22:24:53 +0300

    add scroll top

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,7 @@
 import { bgElements } from "./modules/bg-elements";
 import { modalSearch } from "./modules/modal";
+import { scrollingTop } from "./modules/scroll-top";
 
 bgElements();
 modalSearch();
+scrollingTop();
---
+++ src/pages/modules/scroll-top.js
@@ -0,0 +1,16 @@
+import { scrollIntoView } from "seamless-scroll-polyfill";
+
+export const scrollingTop = () => {
+    const topBtn = document.getElementById("scrollToTopButton");
+    const header = document.querySelector(".header");
+    console.log(header);
+
+    topBtn.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        scrollIntoView(header, {
+            behavior: "smooth",
+            block: "start"
+        });
+    });
+};
